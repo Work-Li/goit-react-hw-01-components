fd225d37441f3c67ccc7f8a0cde127f6e2b24a24
Liliia Bakalinska <lili.lomaka@gmail.com>
2021-12-19 23:52:55 +0200

    FriendsList done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import Profile from './components/Profile/Profile';
 import user from './components/Profile/user.json';
 import Statistics from './components/Statistics/Statistics';
 import data from './components/Statistics/data.json';
+import FriendList from './components/FriendList/FriendList';
+import friends from './components/FriendList/friends.json';
 
 export default function App() {
   return (
@@ -17,6 +19,8 @@ export default function App() {
         likes={user.stats.likes}
       />
       <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
+      <FriendList friends={friends} />
     </>
   );
 }
---
+++ src/components/FriendList/FriendList.js
@@ -0,0 +1,26 @@
+import s from './FriendList.module.css';
+
+import PropTypes from 'prop-types';
+
+export default function FriendList({ friends }) {
+  return (
+    <>
+      <ul className={s.friendList}>
+        {friends.map(friend => (
+          <li key={friend.id} className={s.item}>
+            <span
+              className={s.status}
+              style={{ backgroundColor: friend.isOnline ? 'green' : 'red' }}
+            ></span>
+            <img className={s.avatar} src={friend.avatar} alt="User avatar" width="48" />
+            <p className={s.name}>{friend.name}</p>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(PropTypes.object),
+};
